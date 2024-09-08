06cb196ca4cf885779e74af211c900ac2d19040d
MAA <moprogrammer1384@gmail.com>
2024-09-08 16:44:28 +0330

    Seprating endpoints from web hosting extemsion class

---
+++ Controllers/HomeEndPoints.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+namespace AspNetExercise1.Controllers;
+
+public static class HomeEndPoints
+{
+    public static IEndpointRouteBuilder MapHomeEndPoints
+       (this IEndpointRouteBuilder app)
+    {
+        app.MapGet("/", () => "Welcome to our website!!");
+
+        return app;
+    }
+}
--- HostingExtension/WebHostingExtension.cs
+++ HostingExtension/WebHostingExtension.cs
@@ -1,4 +1,5 @@
-﻿using AspNetExercise1.Infrastructure.Persistence.Extensions;
+﻿using AspNetExercise1.Controllers;
+using AspNetExercise1.Infrastructure.Persistence.Extensions;
 using AspNetExercise1.Middlewares.Extensions;
 using AspNetExercise1.Services.Extensions;
 
@@ -18,8 +19,7 @@ public static class WebHostingExtension
     public static WebApplication ConfigurePipeline(this WebApplication app)
     {
         app.UseRequestInfoSaver();
-        app.MapGet("/", () => "Welcome to our website!!");
-
+        app.MapHomeEndPoints();
 
         return app;
     }
